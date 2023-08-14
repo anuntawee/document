0bdaa09740b7ce0fa9f87512abafea2db1153ecc
anuntawee <56260276+anuntawee@users.noreply.github.com>
2023-08-14 19:59:13 +0700

    first commit

---
+++ menu.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- Style -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>MENU</title>
+</head>
+
+<body>
+
+
+  <div class="d-md-flex half">
+    <div class="bg" style="background-image: url('images/bg_1.jpg');"></div>
+    <div class="contents">
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-12">
+            <div class="form mx-auto ">
+              <div class="text-center mb-5">
+                <h3><strong>บริษัท เชียงใหม่โฟรเซ่นฟูดส์ จำกัดมหาชน</strong></h3>
+               
+              </div>
+          
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
